beece194ed9bb337b790891122815acfa57971c9
mdatsev <martin.datsev.p@gmail.com>
2018-03-21 23:49:34 +0200

    # validate film name uniqueness

--- app/models/film.rb
+++ app/models/film.rb
@@ -2,6 +2,8 @@ class Film < ApplicationRecord
     has_many :cinema_film_connections
     has_many :cinemas, through: :cinema_film_connections
 
+    validates :name, uniqueness: true
+
     validate :cinema_exists
 
     def cinema_exists
