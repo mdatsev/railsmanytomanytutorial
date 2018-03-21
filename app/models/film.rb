36dd8d500fd9fd7bcf291ada696c44bd8b49b03c
mdatsev <martin.datsev.p@gmail.com>
2018-03-21 11:56:51 +0200

    # add cinema validation when creating a film

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,10 @@
 class Film < ApplicationRecord
     has_many :cinema_film_connections
     has_many :cinemas, through: :cinema_film_connections
+
+    validate :cinema_exists
+
+    def cinema_exists
+      errors[:cinema] << "must exist" unless Cinema.exists?(self.cinema_id)
+    end  
 end
