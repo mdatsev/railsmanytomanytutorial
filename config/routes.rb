23172ef14c23174a7da65957091f91abd44fb6bb
mdatsev <martin.datsev.p@gmail.com>
2018-03-21 12:05:47 +0200

    # change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :cinema_film_connections
+  resources :cinema_film_connections, path: '/11b_17_MartinDatsevfilms/'
   resources :cinemas
   resources :films
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
